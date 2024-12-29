d844bddf63ece01f1564b8da43586fd304dbe0f4
okosival <valentine11.dev@gmail.com>
2024-12-29 14:23:52 +0100

    fix: disabled typscript any error

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,55 +1,58 @@
-import { compare } from 'bcrypt-ts';
-import NextAuth, { type User, type Session } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getUser } from '@/lib/db/queries';
+import { compare } from 'bcrypt-ts';
+import NextAuth, { type Session, type User } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 import { authConfig } from './auth.config';
 
 interface ExtendedSession extends Session {
-  user: User;
+    user: User;
 }
 
 export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
 } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      credentials: {},
-      async authorize({ email, password }: any) {
-        const users = await getUser(email);
-        if (users.length === 0) return null;
-        // biome-ignore lint: Forbidden non-null assertion.
-        const passwordsMatch = await compare(password, users[0].password!);
-        if (!passwordsMatch) return null;
-        return users[0] as any;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
+    ...authConfig,
+    providers: [
+        Credentials({
+            credentials: {},
+            async authorize({ email, password }: any) {
+                const users = await getUser(email);
+                if (users.length === 0) return null;
+                // biome-ignore lint: Forbidden non-null assertion.
+                const passwordsMatch = await compare(
+                    password,
+                    users[0].password!,
+                );
+                if (!passwordsMatch) return null;
+                return users[0] as any;
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
 
-      return token;
-    },
-    async session({
-      session,
-      token,
-    }: {
-      session: ExtendedSession;
-      token: any;
-    }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-      }
+            return token;
+        },
+        async session({
+            session,
+            token,
+        }: {
+            session: ExtendedSession;
+            token: any;
+        }) {
+            if (session.user) {
+                session.user.id = token.id as string;
+            }
 
-      return session;
+            return session;
+        },
     },
-  },
 });
