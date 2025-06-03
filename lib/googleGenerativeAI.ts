b7b47fff3ba4e7d6c17cd480323061034a110052
okosival <valentine11.dev@gmail.com>
2025-06-03 06:24:31 +0100

    refactor: remove GoogleGenerativeAI integration to simplify codebase

--- lib/googleGenerativeAI.ts
+++
@@ -1,6 +0,0 @@
-// lib/googleGenerativeAI.ts
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-
-export default genAi;
